b6535372977629aa667498b166b116fc12c3616c
Androrun <106136947+Androrun@users.noreply.github.com>
2024-08-05 01:23:52 -0500

    Update hospedaje.schema.js

--- src/schemas/hospedaje.schema.js
+++ src/schemas/hospedaje.schema.js
@@ -32,12 +32,6 @@ export const createHospedajeSchema = z.object({
       path: ["end_date"],
     })
     .transform((val) => new Date(val)),
-  cost: z
-    .number({
-      required_error: "El costo es requerido",
-      invalid_type_error: "El costo debe ser un número",
-    })
-    .min(0),
   notes: z
     .string()
     .max(1000)
@@ -76,12 +70,6 @@ export const updateHospedajeSchema = z.object({
       path: ["end_date"],
     })
     .transform((val) => new Date(val)),
-  cost: z
-    .number({
-      required_error: "El costo es requerido",
-      invalid_type_error: "El costo debe ser un número",
-    })
-    .min(0),
   notes: z
     .string()
     .max(1000)
