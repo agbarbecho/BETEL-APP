8ea4fa12399e31f5067e8990d47052b0424d573a
Androrun <106136947+Androrun@users.noreply.github.com>
2024-08-05 18:23:48 -0500

    Update DetalleHospitalizacion.jsx

--- BETFRONTEND/src/pages/DetalleHospitalizacion.jsx
+++ BETFRONTEND/src/pages/DetalleHospitalizacion.jsx
@@ -87,19 +87,7 @@ const HospitalizationForm = ({ onClose }) => {
         <label className="block text-gray-700">Dieta del Paciente:</label>
         <input type="text" name="diet" value={formData.diet} onChange={handleChange} className="border rounded p-2 w-full" />
       </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">¿Incluir cobro del servicio?</label>
-        <div>
-          <label className="inline-flex items-center">
-            <input type="radio" name="charge_service" value={true} checked={formData.charge_service === true} onChange={handleChange} />
-            <span className="ml-2">Sí</span>
-          </label>
-          <label className="inline-flex items-center ml-4">
-            <input type="radio" name="charge_service" value={false} checked={formData.charge_service === false} onChange={handleChange} />
-            <span className="ml-2">No</span>
-          </label>
-        </div>
-      </div>
+      
       <div className="flex justify-end">
         <button type="button" onClick={onClose} className="bg-red-500 text-white px-4 py-2 rounded mr-2">Cerrar</button>
         <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Guardar</button>
